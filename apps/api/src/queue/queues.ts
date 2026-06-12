b4b958f8f31deb48cf83308a9017a2a2b621c64f
sgoyal02 <122796484+sgoyal02@users.noreply.github.com>
2026-06-12 20:39:04 +0530

    add: queue const declarations

---
+++ apps/api/src/queue/queues.ts
@@ -0,0 +1,9 @@
+export const QUEUES={
+  THUMBNAIL:   'asset.thumbnail',
+  METADATA:    'asset.metadata',
+  DUPLICATE:   'asset.duplicate',
+  NOTIFICATION:'asset.notification',
+} as const;
+export const EXCHANGES = {
+  ASSET_UPLOADED: 'asset.uploaded',
+} as const;
